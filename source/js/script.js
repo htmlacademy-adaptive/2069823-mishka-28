00df19cc6cd704f05d88ea80258ff3ed6f7db48d
savichev-aa <savichev.pp@gmail.com>
2023-05-03 23:42:09 +0400

    module6-fixbag_6

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,19 @@
 let navMain = document.querySelector('.header-nav');
 let navToggle = document.querySelector('.header__toggle');
 
+navMain.classList.remove('header-nav__no-js');
+
 navToggle.addEventListener('click', function () {
-  navMain.classList.toggle("header-nav__open");
-  navToggle.classList.toggle("header__toggle--open");
+  if (navMain.classList.contains('header-nav__open')) {
+    navMain.classList.remove('header-nav__open');
+    navMain.classList.add('header-nav__closed');
+  } else {
+    navMain.classList.add('header-nav__open');
+    navMain.classList.remove('header-nav__closed');
+  }
 });
+
+
+
+
+
