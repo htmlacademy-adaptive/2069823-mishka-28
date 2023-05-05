14459ed535bb318ed3cc57ad254289bbafcf32f9
savichev-aa <savichev.pp@gmail.com>
2023-05-05 17:10:58 +0400

    оптимизация изображений 3

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,34 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('header-nav__closed');
   }
 });
+
+
+// let page = document.querySelector('.modal-basket');
+// let modalButton = document.querySelector('.product__button');
+
+// modalButton.onclick = function() {
+//   page.classList.toggle('modal-basket__close');
+// };
+
+
+let btnModal = document.querySelector(".product__button");
+let modal= document.querySelector(".modal-basket");
+let toggleMenu = function () {
+    menu.classList.toggle("modal-basket__close");
+}
+
+// btnModal.addEventListener("click", function (e) {
+//     e.stopPropagation();
+//     toggleMenu();
+// });
+
+// document.addEventListener("click", function (e) {
+//     let target = e.target;
+//     let its_menu = target == modal || modal.contains(target);
+//     let its_btnMenu = target == btnModal;
+//     let menu_is_active = modal.classList.contains("modal-basket__close");
+
+//     if (!its_menu && !its_btnMenu && menu_is_active) {
+//         toggleMenu();
+//     }
+// });
